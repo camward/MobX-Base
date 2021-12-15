7fd652d302aca0cf0f7d96c285dc75d65ca81d3a
alex <camward@yandex.ru>
2021-12-15 22:09:36 +0400

    подключение MobX и Ant Design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Layout, Typography } from "antd";
+
 function App() {
+  const { Header, Content, Footer } = Layout;
+  const { Text } = Typography;
+
   return (
-    <div className="App">
-      App
-    </div>
+    <Layout>
+      <Header>
+        <Text type="success">Sprint Board</Text>
+      </Header>
+      <Content>Content</Content>
+      <Footer>MobX, &copy; {new Date().getFullYear()}</Footer>
+    </Layout>
   );
 }
 
